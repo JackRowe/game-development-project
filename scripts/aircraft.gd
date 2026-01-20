f8c06c1b030eae87ca5854c6f6f3a6db168913ec
Jack Rowe <jack.rowe@outlook.com>
2026-01-20 11:46:27 +0000

    attempts

--- scripts/aircraft.gd
+++ scripts/aircraft.gd
@@ -9,12 +9,11 @@ func _physics_process(_delta: float) -> void:
 	var force = global_transform.basis.z * (input.thrustValue * 100)
 	var torque = Vector3.ZERO # Vector3(0.0, 0.0, -0.5).cross(force)
 	
-	print("hello?")
 	for surface in surfaces.get_children():
+		if(surface.name == "Rudder"): continue
 		var result = surface.calculate_forces()
 		force += result[0]
 		torque += result[1]
-		print(surface, "force: ", result[0], " torque: ", result[1])
 	
 	body.apply_central_force(Vector3(force.x / 1000.0, force.y / 1000.0, force.z / 1000.0))
 	body.apply_torque(Vector3(torque.x / 1000.0, torque.y / 1000.0, torque.z / 1000.0))
